bc90dee9142b6e6db6902808e7904c5425b1b490
Phishme <phishme@Phishmes-MacBook-Pro.local>
2015-12-22 10:28:34 -0500

    WIP: Primary navigation rework

--- source/scripts/components/sidr.js
+++ source/scripts/components/sidr.js
@@ -2,11 +2,49 @@
 // Component: Sidr
 // --------------------------------------------------
 
+function sidrMenu() {
+  'use strict';
+  var width = $(window).width();
+  if (width < 768) {
+
+    $('.c-sidr__toggle').sidr({
+      side: 'right'
+    });
+
+  }
+  else {
+
+    $.sidr('close');
+
+  }
+}
+
+// On Load
 ;(function($, window, document, undefined) {
 
   'use strict';
-  $('.c-sidr__toggle').sidr({
-    side: 'right'
-  });
+  sidrMenu();
 
 })(jQuery, window, document);
+
+// On Resize
+$(window).resize(function() {
+
+  'use strict';
+  var delay = (function() {
+    var timer = 0;
+    return function(callback, ms) {
+
+      clearTimeout (timer);
+      timer = setTimeout(callback, ms);
+
+    };
+
+  })();
+
+  delay(function() {
+    sidrMenu();
+  }, 500);
+
+});
+
--- source/views/index.php
+++ source/views/index.php
@@ -1,6 +1,6 @@
 <?php include 'includes/head.inc' ?>
 
-  <body class="p-spec p-spec--overview t-class--rogue">
+  <body class="p-guide t-class--rogue">
     <a href="#main" class="u-skip-to-content">Skip to content</a>
     <div class="u-site-wrapper">
       
@@ -12,7 +12,7 @@
 
         <div class="u-wrapper">
 
-          <?php include 'includes/section-navigation.inc' ?>
+          <?php include 'includes/navigation-section.inc' ?>
 
           <?php include 'content/overview.php' ?>
           <?php include 'content/stats.php' ?>
@@ -26,5 +26,5 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
     <script src="scripts/main.js"></script>
-  </body><!-- /.p-spec -->
+  </body><!-- /.p-guide -->
 </html>
