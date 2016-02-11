883caeb6ac492dacaebe39ea58747b6714ad3594
David Ellinger <david.ellinger@me.com>
2016-02-11 00:36:19 -0800

    Added comment

--- server/models/users.js
+++ server/models/users.js
@@ -7,7 +7,7 @@ User.add({
     namne : {type: Types.Name, required: true, index: true},
     email : {type: Types.Email, initial: true, required: true, index: true},
     password: {type: Types.Password, initial: true},
-    canAccessKeystone: {type: Boolean, initial: true}
+    canAccessKeystone: {type: Boolean, initial: true}   // Field to access Admin UI
 });
 
 User.register();
