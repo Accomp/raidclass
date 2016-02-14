f247c4112f776950ebb96a80af046427b5ff2fed
Ben Sterrett <raynjamin@gmail.com>
2016-02-14 18:54:21 -0500

    Adding Keystone things.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,53 @@
-/**
- * Gulp
- * --------------------------------------------------
- */
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jshintReporter = require('jshint-stylish');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
 
-'use strict';
+var sass = require('gulp-sass');
 
-// Dependencies
-var gulp       = require('gulp');
-var plumber    = require('gulp-plumber');
-var gutil      = require('gulp-util');
-var requireDir = require('require-dir');
 
-// Error handling
-var gulp_src = gulp.src;
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
 
-gulp.src = function() {
-  return gulp_src.apply(gulp, arguments)
-    // Catch errors
-    .pipe(plumber(function(error) {
-      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
-      this.emit('end');
-    }));
+,
+	'style': {
+		all: './public/styles/**/*.scss',
+		output: './public/styles/'
+	}
+
 };
 
-// Load all tasks
-var tasks = requireDir('./gulp_tasks');
+// gulp lint
+gulp.task('lint', function(){
+	gulp.src(paths.src)
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter));
+});
+
+// gulp watcher for lint
+gulp.task('watch:lint', function () {
+	gulp.watch(paths.src, ['lint']);
+});
+
+
+gulp.task('watch:sass', function () {
+	gulp.watch(paths.style.all, ['sass']);
+});
+
+gulp.task('sass', function(){
+	gulp.src(paths.style.all)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(paths.style.output));
+});
+
+
+gulp.task('runKeystone', shell.task('node keystone.js'));
+gulp.task('watch', [
+
+  'watch:sass',
+
+  'watch:lint'
+]);
+
+gulp.task('default', ['watch', 'runKeystone']);
