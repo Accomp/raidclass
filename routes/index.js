4fed7300318ffb52c06a4a24867186142034b47e
Ben Sterrett <raynjamin@gmail.com>
2016-02-22 00:12:51 -0500

    Adding a sample guides route, setting precedent

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,8 @@ exports = module.exports = function(app) {
 	// Views
 	app.get('/', routes.views.index);
 	
-	
+	app.get('/guides/:class?/:spec?', routes.views.guides);
+    
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 	
--- routes/views/guides.js
+++ routes/views/guides.js
@@ -3,7 +3,11 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res);
+    var locals = res.locals;
+    
+    locals.wow_class = req.params.class;
+    locals.spec = req.params.spec;
 	
 	// Render the view
-	view.render('index');
+	view.render('guides');
 };
