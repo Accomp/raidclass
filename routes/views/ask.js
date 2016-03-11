1a8b45ed9d2d83e48c096cb6852b6cab5f6c64e3
David Ellinger <david.ellinger@me.com>
2016-03-10 18:59:16 -0800

    Added ask/blog routes and created html templates for them.

---
+++ routes/views/ask.js
@@ -0,0 +1,13 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+    var locals = res.locals;
+
+    locals.wow_class = req.params.class;
+    locals.spec = req.params.spec;
+
+	// Render the view
+	view.render('ask');
+};
